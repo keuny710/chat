38e4775a71bc84884d1ecf12c936a2592f4e9da8
keuny710 <3ksimon@gamil.com>
2021-07-06 15:04:31 +0900

    Modify app.js & Add app, routes folder

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const flash = require('connect-flash');
 const session = require('express-session');
+const routes = require('./app/routes');
+
 
 require('dotenv').config();
 const { PORT, MONGO_URI } = process.env;
 const mongoose = require('mongoose');
 const passport = require('passport');
 const { initialize } = require('passport');
-
+//view engine setup
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'ejs');
 
+//middlewares setup
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
