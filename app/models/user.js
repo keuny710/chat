569d14ce71c6159e730a1be1a040927900547aa8
keuny710 <3ksimon@gamil.com>
2021-07-14 22:15:17 +0900

    add method

--- app/models/user.js
+++ app/models/user.js
@@ -1,13 +1,44 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const userModel = require('../database').models.user;
 
-const userSchema = new Schema({
-  name: String,
-  email: {
-    type: String, 
-    require: true, 
-    unique: true
-  }
-});
+const create = function(data, callback) {
+  const newUser = new userModel(data);
+  newUser.save(callback);
+};
 
-module.exports = mongoose.model('User', userSchema);
+const findOne = function(data, callback) {
+  userModel.findOne(data, callback);
+};
+
+const findById = function(id, callback) {
+  userModel.findById(id, callback);
+};
+
+const findOrCreate = function(data, callback){
+  findOne({'socialId': data.id}, function(err, user){
+    if(err) {
+      return callback(err);
+    } else{
+      const userData = {
+        username: data.displayName,
+        socialId: data.id,
+        picture: data.photos[0].value || null
+      };
+      // 200px x 200px의 프로필 사진을 요청
+      if(data.provider == 'facebook' && userData.picture){
+        userData.picture = "http://graph.facebook.com/" + data.id + "picture/?type=large"
+      }
+
+      create(userData, function(err, newUser){
+        callback(err, newUser);
+      });
+    }
+  });
+}
+
+const isAuthenticated = function (req, res, next) {
+	if(req.isAuthenticated()){
+		next();
+	}else{
+		res.redirect('/');
+	}
+}
