c9965e9294137cf846cc2e9ff654a6c79666ff34
keuny710 <3ksimon@gamil.com>
2021-07-15 14:24:29 +0900

    add room method

--- app/models/room.js
+++ app/models/room.js
@@ -10,7 +10,7 @@ const find = function(data, callback) {
     roomModel.find(data, callback);
 }
 
-const findOne = funtion(data, callback) {
+const findOne = function(data, callback) {
     roomModel.findOne(data, callback);
 }
 
@@ -29,3 +29,74 @@ const addUser = function(room, socket, callback) {
     room.connections.push(conn);
     room.save(callback);
 }
+
+const getUsers = function(room, socket, callback) {
+    const users = [];
+    const vis = {};
+    const count = 0;
+    const userId = socket.request.session.passport.user;
+
+    room.connections.forEach(function(conn) {
+        if(conn.userId === userId) {
+            count++;
+        }
+        if(!vis[conn.userId]) {
+            users.push(conn.userId);
+        }
+        vis[conn.userId] = true;
+    });
+
+    const loadedUsers = 0;
+    users.forEach(function(userId, i) {
+        User.findById(userId, function(err, user) {
+            if(err) {
+                return callback(err);
+            }
+            users[i] = user;
+
+            if(++loadedUsers === users.length) {
+                return callback(null, users, count);
+            }
+        });
+    });
+}
+
+const removeUser = function(socket, callback) {
+    const userId = socket.request.session.passport.user;
+
+    find(function(err, rooms) {
+        if(err) {
+            return callback(err);
+        }
+        rooms.every(function(room) {
+            const pass = true;
+            const count = 0;
+            const target = 0;
+
+            room.connections.forEach(function(conn, i) {
+                if(conn.userId === userId) {
+                    count++;
+                }
+                if(conn.socketId === socket.id) {
+                    pass = false, target = i;
+                }
+            });
+            if(!pass) {
+                room.connections.id(room.connections[target]._id).remove();
+                room.save(function(err) {
+                    callback(err, room, userId, count);
+                });
+            }
+            return pass;
+        });
+    });
+}
+
+module.exports = {
+    create,
+    find,
+    findOne,
+    findById,
+    getUsers,
+    removeUser
+};
