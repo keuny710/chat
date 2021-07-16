fada4f8a08b276cdb15b5dc90475917d3cb472b7
keuny710 <3ksimon@gamil.com>
2021-07-16 18:37:55 +0900

    add modules

--- app.js
+++ app.js
@@ -3,22 +3,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const bodyParser = require('body-parser');
 const flash = require('connect-flash');
-const session = require('express-session');
 
+const routes = require('./app/routes');
+const session = require('./app/sessions');
+const passport = require('./app/auth');
+const ioServer = require('./app/socket');
+const logger = require('./app/logger');
 
 require('dotenv').config();
 const { PORT} = process.env;
-const mongoose = require('mongoose');
-const passport = require('passport');
-const { initialize } = require('passport');
-
-const routes = require('./app/routes');
-const webSocket = require('./app/socket');
-const connect = require('./app/database');
-
-
 //view engine setup
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'html');
@@ -34,16 +28,15 @@ connect();
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false,
-    },
-}));
+// app.use(session({
+//     resave: false,
+//     saveUninitialized: false,
+//     secret: process.env.COOKIE_SECRET,
+//     cookie: {
+//         httpOnly: true,
+//         secure: false,
+//     },
+// }));
 app.use(passport(initialize()));
 app.use(passport.session());
 app.use(flash());
@@ -54,3 +47,5 @@ app.use((req, res, next)=>{
     res.status(404).sendFile(process.cwd()+'/app/views/404.html');
 });
 
+
+ioServer.listen(port);
